d6e09e609006f13050120f6dc67ff83506c5fbd5
Seu Nome <seu.email@exemplo.com>
2025-10-23 10:48:20 -0300

    fix: remove payment section banner completely

--- apps/web/src/pages/Fees.tsx
+++ apps/web/src/pages/Fees.tsx
@@ -93,7 +93,7 @@ export default function Fees() {
 
         <FeeTable fees={fees} />
 
-        {/* Deploy trigger - $(Get-Date) - Payment section removed - UCS links working */}
+        {/* PAYMENT SECTION COMPLETELY REMOVED - UCS LINKS WORKING */}
 
         <div className="mt-12 max-w-4xl mx-auto prose prose-lg">
           <h3>{t("fees.included")}</h3>
